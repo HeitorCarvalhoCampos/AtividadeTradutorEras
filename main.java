9b6da3013152c738973b25c176486a4cdacded15
HeitorCarvalhoCampos <hcarvalhocampos2008@gmail.com>
2026-06-13 14:50:13 -0300

    refactor: improve code structure and formatting in main.java

--- main.java
+++ main.java
@@ -1,28 +1,29 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Comparator;
 
 public class main {
 
-    List<Motorista> motoristas = getMotoristasDaEmpresa();
+    public void executar() {
+        List<Motorista> motoristas = getMotoristasDaEmpresa();
 
-    List<Motorista> habilitados = new ArrayList<>();
+        List<Motorista> habilitados = new ArrayList<>();
 
-    // Filtrando motoristas com CNH ativa
-    for(
-    Motorista m:motoristas)
-    {
-        if (m.isCnhAtiva()) {
-            habilitados.add(m);
+        // Filtrando motoristas com CNH ativa
+        for (Motorista m : motoristas) {
+            if (m.isCnhAtiva()) {
+                habilitados.add(m);
+            }
         }
-    }
 
-    // Ordenando por tempo de casa (anos) usando Comparador anônimo
-    Collections.sort(habilitados,new Comparator<Motorista>(){
-
-    @Override
-    public int compare(Motorista m1, Motorista m2) {
-        return Integer.compare(m1.getAnosEmpresa(), m2.getAnosEmpresa());
+        // Ordenando por tempo de casa (anos) usando Comparador anônimo
+        Collections.sort(habilitados, new Comparator<Motorista>() {
+            @Override
+            public int compare(Motorista m1, Motorista m2) {
+                return Integer.compare(m1.getAnosEmpresa(), m2.getAnosEmpresa());
+            }
+        });
     }
-});
 
 }
